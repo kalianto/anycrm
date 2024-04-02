de489afe1f0988c0ae28773ec0305abc36ffb4e2
Kalianto Wong <kalianto@gmail.com>
2024-04-02 12:40:32 +1000

    first login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,30 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+import { GeistSans } from 'geist/font/sans';
+import type { Metadata } from 'next';
+import './globals.css';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : 'https://localhost:3000';
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: 'AnyCRM Portal',
+  description:
+    'C stands for Customer, Client, Congregation, Church, Charity, Corporate, Company',
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+    <html lang='en' className={GeistSans.className}>
+      <body
+        className='bg-background text-foreground'
+        suppressHydrationWarning={true}
+      >
+        <main className=''>{children}</main>
       </body>
     </html>
   );
