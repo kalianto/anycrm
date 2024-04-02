31422a970e657f5644ba65263c9991b334c0bf2f
Kalianto Wong <kalianto@gmail.com>
2024-04-02 23:21:42 +1000

    added signin using nextauth, google and facebook, with tailwind-css and shadcn-ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import { GeistSans } from 'geist/font/sans';
 import type { Metadata } from 'next';
 import './globals.css';
+import { auth } from '../auth';
+import { LoginPage } from '@/components/Login';
+import { SessionProvider } from 'next-auth/react';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -13,18 +16,36 @@ export const metadata: Metadata = {
     'C stands for Customer, Client, Congregation, Church, Charity, Corporate, Company',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  console.log("🚀🚀🚀 ~ file: layout.tsx:25 ~ session:", session);
+
+  if (!session) {
+    return (
+      <html lang='en' className={GeistSans.className}>
+      <body
+        className='bg-background text-foreground'
+        suppressHydrationWarning={true}
+      >
+        <LoginPage />
+      </body>
+    </html>
+    )
+  }
+
   return (
     <html lang='en' className={GeistSans.className}>
       <body
         className='bg-background text-foreground'
         suppressHydrationWarning={true}
       >
-        <main className=''>{children}</main>
+        <SessionProvider session={session}>
+          <main className=''>{children}</main>
+        </SessionProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,5 @@
-import { createClient } from '@/utils/supabase/server';
-import ConnectSupabaseSteps from '@/components/tutorial/ConnectSupabaseSteps';
-import SignUpUserSteps from '@/components/tutorial/SignUpUserSteps';
-import Header from '@/components/Header';
-import { LoginPage } from '@/components/Login';
-
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
   return (
-    <div className='h-full w-full max-h-full max-w-full'>
-      {isSupabaseConnected && <LoginPage />}
-    </div>
+    <div className='h-full w-full max-h-full max-w-full'>Welcome Pseudo ME</div>
   );
 }
